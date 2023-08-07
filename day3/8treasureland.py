743e2544cee6470224a6f3ec4c3f62595523b070
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-07 22:22:34 +0530

    finalupdate

--- day3/8treasureland.py
+++ day3/8treasureland.py
@@ -23,4 +23,22 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
       ''')
 
 print("Welcome to Treasure Island.Your mission is to find the treasure.")
-dir = input(print("Choose left or right"))
+dir = input(
+    'You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if dir == "left":
+    ip = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if ip == "wait":
+        door = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        if door == "red":
+            print("Burned by fire.Game Over.")
+        elif door == "blue":
+            print("Eaten by beasts.Game Over.")
+        elif door == "yellow":
+            print("You Win!")
+        else:
+            print("Game Over")
+    else:
+        print("Attacked by trout Game Over.")
+else:
+    print("Fall into a hole.Game Over.")
