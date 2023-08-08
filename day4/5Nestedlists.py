4f6fd29ffc04666cb3caed60e6fd7f62c714ffbd
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-08 13:39:51 +0530

    5up

---
+++ day4/5Nestedlists.py
@@ -0,0 +1,10 @@
+'''Nested lists'''
+
+fruits = ["Strawberries", "rappples", "grapes", "peaches", "cherries"]
+
+veggies = ["spinach", "kale", "tomatoes", "celery", "potatoes"]
+
+
+final_list = [fruits, veggies]
+
+print(final_list)
