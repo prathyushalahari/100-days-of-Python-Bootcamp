99a21a4fbf8ccd95a47eb18d9201789b2f6da804
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-09 16:03:06 +0530

    up

---
+++ day5/1forlloop.py
@@ -0,0 +1,7 @@
+'''SIMPLE FOR LOOP'''
+
+fruits = ["Apple", "Peach", "Pear"]
+for element in fruits:
+    print(element)  # element
+    print(element + " Pie")  # element+string
+    print(fruits)  # list in forloop
