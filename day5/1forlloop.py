bc7043d19a177aa6c295fc2a9568309a8064fbf0
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-11 12:19:39 +0530

    day5_1

--- day5/1forlloop.py
+++ day5/1forlloop.py
@@ -5,3 +5,17 @@ for element in fruits:
     print(element)  # element
     print(element + " Pie")  # element+string
     print(fruits)  # list in forloop
+
+# looping in range
+for number in range(1, 11):
+    print(number)
+
+# looping using range and step
+for number in range(1, 11, 3):
+    print(number)
+
+# sum of given range
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
---
+++ day5/4even.py
@@ -0,0 +1,7 @@
+'''Exercise 3 - Adding Even Numbers '''
+'''https://app.codingrooms.com/w/Sm8oyELA1cju'''
+
+total = 0
+for number in range(2, 101, 2):
+    total += number
+print(total)
