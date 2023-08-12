77aee7e378cf6f316b3634870700dfdd0e8090ca
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-12 22:30:59 +0530

    up

---
+++ day5/5FizzBuzz.py
@@ -0,0 +1,13 @@
+'''Exercise 4 - FizzBuzz
+https://app.codingrooms.com/management/assignments/364939/overview'''
+
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0 and number % 5 != 0:
+        print("Fizz")
+    elif number % 5 == 0 and number % 3 != 0:
+        print("Buzz")
+    else:
+        print(number)
