a25052737de3cd09394de88cc6e9dac25d98567e
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-19 23:00:33 +0530

    up

---
+++ day7/2challengeword.py
@@ -0,0 +1,18 @@
+
+
+import random
+
+
+word_list = ["ardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+print(f'Psst,the solution is{chosen_word}.')
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
