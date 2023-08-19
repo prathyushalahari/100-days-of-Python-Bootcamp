01f5ac04b5c3ecba5ed9e03efdc8ec9da586f2f0
Prathyusha Lahari <m.prathyushalahari@gmail.com>
2023-08-19 20:31:35 +0530

    up

--- day7/1challenge.py
+++ day7/1challenge.py
@@ -1,3 +1,14 @@
-# step 1
+# guesses whether user input letter present in words present in list
 
+import random
 word_list = ["advark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+guess = input("Guess a letter:").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
